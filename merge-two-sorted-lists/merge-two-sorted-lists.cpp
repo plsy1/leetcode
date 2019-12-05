69d82a5715ddf2275b6e7852c779e9c5b0eac1bd
Ztyo <39407459+fz0325@users.noreply.github.com>
2019-12-05 20:35:42 +0800

    迭代

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,18 @@
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+  ListNode* res = new ListNode(-1);
+  ListNode* prev = res;
+
+  while(l1 != NULL && l2 != NULL) {
+    if(l1->val <= l2->val) {
+      res->next = l1;
+      l1 = l1->next;
+    }
+    else {
+      res->next = l2;
+      l2 = l2->next;      
+    }
+    res = res->next;
+  }
+  res->next = l1->next != NULL ? l1 : l2;
+  return prev->next;
+}
