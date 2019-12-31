259a2ee1b2f03211c995d1fed186d1fe0a7baf32
fz0325 <1601977399@qq.com>
2019-12-31 16:58:30 +0800

    no message

---
+++ 069.sqrtx/069.sqrtx.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int mySqrt(int x) {
+  int y = x;
+  while(y * y > x)
+    y = (y + x / y) / 2;
+  return y;
+}
+
+int main(int argc, char const *argv[]) {
+  int res = mySqrt(7);
+  cout << res;
+  return 0;
+}
---
+++ 118.submissions/118.submissions.cpp
@@ -0,0 +1,14 @@
+vector<vector<int>> generate(int numRows) {
+  vector<vector<int>> nums(numRows);
+  if(numRows == 0)
+    return nums;
+  for(int i = 0;i != numRows;++i) {
+    for(int j = 0;j <= i;++j ) {
+      if(j == 0 || j == i)
+        nums[i].push_back(1);
+      else
+        nums[i].push_back(nums[i-1][j-1] + nums[i-1][j]);
+    }
+  }
+  return nums;
+}
